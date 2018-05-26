23eecb5c3003c5c1f245d7d553246a3e625fdf4a
hanastrickland <strickh@uw.edu>
2018-05-26 09:58:26 -0700

    put sidebar layout inside tab:

--- my_ui.R
+++ my_ui.R
@@ -3,27 +3,36 @@ ui <- fluidPage(
   
   titlePanel("TITLE"),
   
-  sidebarLayout(
-    sidebarPanel(
-      # Sidebar panel
-    ),
+  # sidebarLayout(
+  #   sidebarPanel(
+  #     # Sidebar panel
+  #   ),
+  #   
+  #   mainPanel(
+  #     
+  #     # Main panel
+  #   
+  #     
+  #     tabsetPanel(
+  #       type = "tabs",
+  #       tabPanel("Question 1", plotOutput("plot1a"),plotOutput("plot1b"), plotOutput("plot1c") ),
+  #       tabPanel("Question 2", plotlyOutput("plot2"))
+  #     )
+  #     )
+  #   )
     
-    mainPanel(
-      
-      # Main panel
-    
-      
-      tabsetPanel(
-        type = "tabs",
-        tabPanel("Question 1", plotOutput("plot1a"),plotOutput("plot1b"), plotOutput("plot1c") ),
-        tabPanel("Question 2", plotlyOutput("plot2"))
-      )
-      )
-    )
-    
-    
+  tabsetPanel(
+    type = "tabs",
+    tabPanel("Question 1", 
+             sidebarLayout(
+               sidebarPanel("Sidebar Panel"),
+               mainPanel(plotOutput("plot1a"),plotOutput("plot1b"), plotOutput("plot1c") )
+                           ) 
+             ),
+    tabPanel("Question 2", plotlyOutput("plot2"))
     
     
     
   )
+)
 
