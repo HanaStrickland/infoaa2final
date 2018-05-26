e510b58130826c19db3e6ac0d8fdff9587d4fa28
hanastrickland <strickh@uw.edu>
2018-05-26 09:39:15 -0700

    change navbar to tabset panel

--- my_ui.R
+++ my_ui.R
@@ -11,11 +11,13 @@ ui <- fluidPage(
     mainPanel(
       
       # Main panel
-      navbarPage(tabPanel("Question 0"), # I don't know why it won't show the first tab pannel
-                 tabPanel("Question 1", plotOutput("plot1")),
-                 tabPanel("Question 2"), plotlyOutput("plot2")), #shows up on first tab too for some reason
-                 tabPanel("Question 3"),
-                 tabPanel("Question 4")
+    
+      
+      tabsetPanel(
+        type = "tabs",
+        tabPanel("Question 1", plotOutput("plot1")),
+        tabPanel("Question 2", plotlyOutput("plot2"))
+      )
       )
     )
     
