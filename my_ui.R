e210081f54102b1270452d6777ab08beebc3a8e0
hanastrickland <strickh@uw.edu>
2018-05-27 09:48:46 -0700

    edit layout

--- my_ui.R
+++ my_ui.R
@@ -1,5 +1,5 @@
 ui <- fluidPage(
-  titlePanel("TITLE"),
+  titlePanel("Analysis of the Relationship Between Life Expectancy and Race"),
   tabsetPanel(
     type = "tabs",
     tabPanel("Question 1", 
@@ -19,16 +19,28 @@ ui <- fluidPage(
                mainPanel(
                )
              )
+    ),
+    tabPanel("Question 3",
+             sidebarLayout(
+               sidebarPanel(
+                 
+               ),
+               mainPanel(
+               )
+             )
+    ),
+    tabPanel("Question 4",
+             sidebarLayout(
+               sidebarPanel(
+                 
+               ),
+               mainPanel(
+               )
+             )
     )
     
     
     
   )
-  
-  
-  
-  
-  
-  
-)
+
 )
