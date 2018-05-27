b96796d67e4b092d2d121221a0b7010dc3d2f95b
hanastrickland <strickh@uw.edu>
2018-05-26 18:48:21 -0700

    add scatterplot comparing races and table for Q2

--- my_server.R
+++ my_server.R
@@ -3,13 +3,33 @@ server <- function(input, output) {
   
   
   # Server stuff
+  
+  ################## 
+  ### Question 1 ###
+  ##################
+  
+  
+  results_data1 <- reactive({
+    
+    results <- income_by_le[income_by_le$Year == input$year, ]
+    
+    results
+    
+    
+  })
+  
+  results_data2 <- reactive({
+    results <- le_at_birth_race_long[le_at_birth_race_long$Location == input$location, ]
+    results
+  })
+  
   output$plot1a <- renderPlot({
 
     plot1a <- ggplot(data = income_by_le) +
       geom_point(mapping = aes(x = Avg.Life.Expectancy.Years, y = median_income,  color = Race)) +
       labs(title = "Relationship Between Average Life Expectancy and Median Income",
             x = "Average Life Expectancy",
-            y = "Median Income")
+            y = "Median Income") 
     plot1a
   })
   
@@ -33,15 +53,45 @@ server <- function(input, output) {
     plot1c
   })
   
+  output$table1 <- renderDataTable({
+    get_result <- results_data1()
+    get_result
+  })
+  
+  ################## 
+  ### Question 2 ###
+  ##################
+  
   output$plot2 <- renderPlotly({
-    
+
+
     plot2 <- ggplot(data = change) +
       geom_polygon(aes(x = long, y = lat, group = group, fill =
                          cut(new_data$African.American, breaks = 4))) +
-      scale_fill_manual(values = c("#dd3497", "#ae017e", "#7a0177", "#49006a"), na.value = "#f0f0f0") 
+      scale_fill_manual(values = c("#dd3497", "#ae017e", "#7a0177", "#49006a"), na.value = "#f0f0f0")
+
     plot2 + geom_point()
     ggplotly(plot2, tooltip="region")
   })
+  
+  output$plot2b <- renderPlot({
+
+    ggplot(data = results_data2(), aes(x = Race, y = Life_Expectancy, color = Race)) +
+      geom_point(na.rm = TRUE) +
+      labs(title = "Relationship Between Race and Life Expectancy",
+           x = "Race",
+           y = "Life Expectancy")
+    
+    
+  })
+
+  output$table2 <- renderDataTable({
+    results_data2()
+  })
+  
+  
+  
+  
 }
 
 
--- my_ui.R
+++ my_ui.R
@@ -1,5 +1,6 @@
 
 ui <- fluidPage(
+  source("data_wrangling.R"),
   
   titlePanel("TITLE"),
   
@@ -20,19 +21,43 @@ ui <- fluidPage(
   #     )
   #     )
   #   )
-    
+  
   tabsetPanel(
     type = "tabs",
     tabPanel("Question 1", 
              sidebarLayout(
-               sidebarPanel("Sidebar Panel"),
-               mainPanel(plotOutput("plot1a"),plotOutput("plot1b"), plotOutput("plot1c") )
-                           ) 
-             ),
-    tabPanel("Question 2", plotlyOutput("plot2"))
+               sidebarPanel(
+                 selectInput("year", label = "Select Year for Table", choices = years)
+
+                            ),
+               mainPanel(plotOutput("plot1a"),
+                         plotOutput("plot1b"), 
+                         plotOutput("plot1c"),
+                         dataTableOutput("table1"))
+             ) 
+    ),
+    tabPanel("Question 2",
+             sidebarLayout(
+               sidebarPanel(
+                 selectInput("location", label = "Select Location (for Plot 2)", choices = locations)
+                 
+               ),
+               mainPanel(
+                 plotlyOutput("plot2"),
+                 plotOutput("plot2b"),
+                 dataTableOutput("table2")
+                 )
+             )
+    )
     
     
     
   )
+  
+  
+  
+  
+  
+  
 )
 
