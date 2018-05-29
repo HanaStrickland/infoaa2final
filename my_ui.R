ca5551d66a9e7536c6a345fe967d4353826a57f8
Sebastian Torres Retana <storres2@uw.edu>
2018-05-29 03:24:12 -0700

    Fixed minor error for question 3 visualizations where plots 3a and 3b wouldn't show up

--- my_ui.R
+++ my_ui.R
@@ -5,8 +5,11 @@ ui <- fluidPage(
     ## conditionalPanel() functions for selected tab
     conditionalPanel(condition = "input.tabselected==1"),
     conditionalPanel(condition = "input.tabselected==2",
-                     radioButtons("choice","Choose a Race", choices = c("White" = 1, "African American" = 2,
-                                                                        "Asian American" = 3, "Native American" = 4, "Latino" = 5 ))),
+                     radioButtons("choice","Choose a Race", choices = c("White" = 1,
+                                                                        "African American" = 2,
+                                                                        "Asian American" = 3,
+                                                                        "Native American" = 4,
+                                                                        "Latino" = 5 ))),
                  
     conditionalPanel(condition = "input.tabselected==3",
                     radioButtons("choice2", "Choose a Race", choices = c("White" = 1, "Black" = 2, "Hispanic" = 3)))
@@ -17,20 +20,26 @@ ui <- fluidPage(
     # id argument is important in the tabsetPanel()
     # value argument is important in the tabPanle()
     tabsetPanel(
-      tabPanel("Question 1", value=1, plotOutput("plot1a"), plotOutput("plot1b"), plotOutput("plot1c") ),
+      tabPanel("Question 1", value=1, 
+               plotOutput("plot1a"),
+               plotOutput("plot1b"), 
+               plotOutput("plot1c")),
       
+      # Question 2 Plots
       tabPanel("Question 2", value=2, 
                conditionalPanel(condition="input.choice==1", plotlyOutput("plot2white")),
                conditionalPanel(condition="input.choice==2", plotlyOutput("plot2afa")),
                conditionalPanel(condition="input.choice==3", plotlyOutput("plot2asa")),
                conditionalPanel(condition="input.choice==4", plotlyOutput("plot2na")),
                conditionalPanel(condition="input.choice==5", plotlyOutput("plot2lat"))),
+               
       
       # Question 3 Plots
       tabPanel("Question 3", value=3,
                conditionalPanel(condition="input.choice2==1", plotlyOutput("plot3cw")),
                conditionalPanel(condition="input.choice2==2", plotlyOutput("plot3cb")),
-               conditionalPanel(condition="input.choice2==3", plotlyOutput("plot3ch"))),
+               conditionalPanel(condition="input.choice2==3", plotlyOutput("plot3ch")),
+               plotOutput("plot3a"), plotOutput("plot3b")),
       
       id = "tabselected"
     )
