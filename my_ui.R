27bb305c18e5c0805300b562e4dc1a314e019a56
kelsiej <37823350+kelsiej@users.noreply.github.com>
2018-05-31 13:14:04 -0700

    Update my_ui.R

--- my_ui.R
+++ my_ui.R
@@ -125,7 +125,9 @@ For all races, average median income and life expectancy is ", avg_median_income
                                   tabPanel(strong("Heat Map of Life Expectancy"), value = 2, align = "center",
                                            conditionalPanel(condition = "input.choice==1", plotlyOutput("plot2white", height = "700px"), 
                                                             HTML(
-                                                              '<br><p align = "left">Average Age Range for Whites: 77.5-80</p><h3>Facts About Life Expectancy For Each Race in 2008</h3><br>
+                                                              '<br><p align = "left">Average Age Range for Whites: 77.5-80</p>
+                                  <br><p> Whites in the U.S.A have among the highest life expectancies of all races. This is likely due to them having the least uninsured population, with only 5.7% of whites being uninsured.</p><br>
+                                  <h3>Facts About Life Expectancy For Each Race in 2008</h3><br>
                                   <table style="width:80%" align="left">
                                   <tr>
                                   <td><p style="font-size:160%;">Average Age in General</p></td>
